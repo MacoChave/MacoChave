ee023ba8ccffb18bbb02e1a3d07cf76d7c43fdf3
MacoChave <201020831@ingenieria.usac.edu.gt>
2021-07-11 01:40:04 -0600

    Menu mobile & desktop design redefined

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,10 @@
 let main_menu = document.querySelector('#toggleMenu');
-main_menu.addEventListener('click', () => {
-    let menu = document.querySelector('#menu');
-    menu.classList.toggle('hide-menu');
-});
+main_menu.addEventListener('click', hideMenu);
+
+function hideMenu() {
+    if (window.innerWidth < 426) {
+        let menu = document.querySelector('#menu');
+        menu.classList.toggle('menu-container--hide');
+        main_menu.classList.toggle('toggle-menu--hide');
+    }
+}
